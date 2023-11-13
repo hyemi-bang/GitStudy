3a97184a04ef4def893bfa41c97f1e148da6f691
Gi7E-36 <Gi7E-36@Gi7A-21>
2023-11-13 11:16:38 +0900

    Create Banana class

---
+++ GitStudy/src/fruits/Banana.java
@@ -0,0 +1,17 @@
+package fruits;
+
+public class Banana {
+
+	int qty;
+	String brand;
+	int price;
+	
+	public String getBrand() {
+		return brand;
+	}
+	
+	public int getQty() {
+		return qty;
+	}
+	
+}
