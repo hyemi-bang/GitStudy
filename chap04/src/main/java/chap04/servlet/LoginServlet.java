bc732815657c5600dd05753a1d06c3f958a4ae43
hyemi-bang <pbhm1124@gmail.com>
2023-12-27 14:59:07 +0900

    Quiz explanation

--- chap04/src/main/java/chap04/servlet/LoginServlet.java
+++ chap04/src/main/java/chap04/servlet/LoginServlet.java
@@ -1,8 +1,8 @@
 package chap04.servlet;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
@@ -12,47 +12,38 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
-
-@WebServlet("/chap04/login")
+@WebServlet("/quiz2/login")
 public class LoginServlet extends HttpServlet {
-	
+
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		String userId = req.getParameter("user-id");
-		String userPassword = req.getParameter("user-password");
+		//JSP 내장객체, 자동으로 만들어져 있는 객체들 (ServletContext, HttpSession 등등)
+		ServletContext application = req.getServletContext();
+		HttpSession session = req.getSession();
 		
-		boolean isValidMember = validateMember(userId, userPassword);
+		String id = req.getParameter("id");
+		String pwd = req.getParameter("pwd");
 		
-		if (isValidMember) {
-			// 세션 생성 및 사용자 정보 저장
-			HttpSession session = req.getSession(true);
-            session.setAttribute("userId", userId);
-            
-            List<User> registeredUsers = UserDataManager.getRegisteredUsers();
-      
-            for (User user : registeredUsers) {
-                if (user.getUsername().equals(userId) && user.getPassword().equals(userPassword)) {
-                    // 로그인 성공 시, 현재 사용자 목록을 ServletContext에 저장
-                    storeUsersInServletContext(req.getServletContext(), registeredUsers);
-                    // 로그인 성공 시
-                    resp.sendRedirect("/chap04/session/userPage.jsp");
-                    return;
-                }
-            }
-            
+		//String pwd2 = (String)application.getAttribute(id);
+		Map<String, Object> infoMap = (Map)application.getAttribute(id);
+		String pwd2 = (String) infoMap.get("pwd");
+		
+		//ID가 존재하고 해당 ID에 비밀번호를 맞추면 로그인 성공
+		if(pwd2 != null && pwd.equals(pwd2)) {
+			//Map<String, String> infoMap = new HashMap<>();
+			infoMap.put("login", "ok");
+			
+			//Attribute는 Context내부에서 활용하는 값이기 때문에 모든 자바 객체 타입을 값으로 활용할 수 있다 (String , List, 내가만든회원정보객체 등등)
+			session.setAttribute("info", infoMap); 
+						//"info" 로그인 한 정보 등 기록하기 위함
+						// 1=> 로그인한 사용자에게 환경설정 해주는 것 
+						// 유트브 프리미엄 가입자들에게 광고가 안보이도록 "{premium :true, age~~~ 등등 정보를 넣을 수 있음}"
+			resp.sendRedirect("/chap04/quiz2/memo/main");
+			
 		} else {
-			PrintWriter out = resp.getWriter();
-			out.print("<html><body><p>로그인 실패. 아이디/비밀번호를 다시 확인하세요.</p></body></html>");
+			//로그인실패
+			resp.sendRedirect("/chap04/quiz2/index.jsp?error=1");
+			
 		}
 	}
-	
-	private boolean validateMember(String userId, String userPassword) {
-		// 여기서는 간단하게 하드코딩한 예제를 사용, 실제 프로젝트에서는 DB 등을 활용
-		return "userId".equals(userId) && "userPassword".equals(userPassword);
-	}
-	
-	private void storeUsersInServletContext(ServletContext servletContext, List<User> users) {
-        servletContext.setAttribute("registeredUsers", users);
-    }
 }
